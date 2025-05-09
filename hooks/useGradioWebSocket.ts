3e80882711ac638e68de45cd2a28a339964bc6c8
Adnank999 <adnanhossain4ever@gmail.com>
2025-05-09 17:16:13 +0600

    update changes

--- hooks/useGradioWebSocket.ts
+++ hooks/useGradioWebSocket.ts
@@ -5,7 +5,7 @@ export function useGradioWebSocket(url: string) {
     const [message, setMessage] = useState<string | null>(null);
     const socketRef = useRef<WebSocket | null>(null);
 
-    console.log("connected to web socket secure",connected)
+    console.log("connected to web socket secure 2",connected)
     useEffect(() => {
         const ws = new window.WebSocket(url);
         socketRef.current = ws;
